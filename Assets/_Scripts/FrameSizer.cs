3aff4158bd391ed863f88490b75300fadff97c14
selimbat <selim.tirellil@student.ecp.fr>
2019-06-01 20:31:02 +0200

    bugfix frames initialisation

--- Assets/_Scripts/FrameSizer.cs
+++ Assets/_Scripts/FrameSizer.cs
@@ -19,11 +19,17 @@ public class FrameSizer : MonoBehaviour
 	private Vector2 _frameCenter;
 	private const float NORMAL_SCALE = 10;
 
-	private void Awake()
+	[Button]
+	private void Init()
 	{
+		_top.localPosition = new Vector3(92, 191, 0);
+		_right.localPosition = new Vector3(190, 93, 0);
+		_bottom.localPosition = new Vector3(92, -4, 0);
+		_left.localPosition = new Vector3(-5, 93, 0);
 		_frameSide = _top.localPosition.y - _bottom.localPosition.y;
-		_borderHalfWidth = Mathf.Abs(_bottom.localPosition.y);
+		_borderHalfWidth = 3.5f;
 		_frameCenter = (_top.localPosition + _bottom.localPosition) / 2;
+		Debug.DrawLine(transform.position, _frameCenter);
 	}
 
 	private void ScaleXAxis()
@@ -31,9 +37,17 @@ public class FrameSizer : MonoBehaviour
 		_top.localScale = new Vector3(_xScale, _top.localScale.y, _top.localScale.z);
 		_bottom.localScale = new Vector3(_xScale, _bottom.localScale.y, _bottom.localScale.z);
 
-		_right.localPosition = _frameCenter + _xScale / NORMAL_SCALE * (_borderHalfWidth + _frameSide / 2) * Vector2.right;
-		_left.localPosition = _frameCenter + _xScale / NORMAL_SCALE * (_borderHalfWidth + _frameSide / 2) * Vector2.left;
-		Debug.Log(_borderHalfWidth);
+		float scaleRatio = _xScale / NORMAL_SCALE;
+		if (scaleRatio > 1)
+		{
+			_right.localPosition = _frameCenter + scaleRatio * (_borderHalfWidth + _frameSide / 2) * Vector2.right;
+			_left.localPosition = _frameCenter + scaleRatio * (_borderHalfWidth + _frameSide / 2) * Vector2.left;
+		}
+		else
+		{
+			_right.localPosition = _frameCenter + scaleRatio *  _frameSide / 2 * Vector2.right;
+			_left.localPosition = _frameCenter + scaleRatio *  _frameSide / 2 * Vector2.left;
+		}
 	}
 
 	private void ScaleYAxis()
@@ -41,7 +55,16 @@ public class FrameSizer : MonoBehaviour
 		_right.localScale = new Vector3(_yScale, _right.localScale.y, _right.localScale.z);
 		_left.localScale = new Vector3(_yScale, _left.localScale.y, _left.localScale.z);
 
-		_top.localPosition = _frameCenter + _yScale / NORMAL_SCALE * (_borderHalfWidth + _frameSide / 2) * Vector2.up;
-		_bottom.localPosition = _frameCenter + _yScale / NORMAL_SCALE * (_borderHalfWidth + _frameSide / 2) * Vector2.down;
+		float scaleRatio = _yScale / NORMAL_SCALE;
+		if (scaleRatio > 1)
+		{
+			_top.localPosition = _frameCenter + scaleRatio * (_borderHalfWidth + _frameSide / 2) * Vector2.up;
+			_bottom.localPosition = _frameCenter + scaleRatio * (_borderHalfWidth + _frameSide / 2) * Vector2.down;
+		}
+		else
+		{
+			_top.localPosition = _frameCenter + scaleRatio *  _frameSide / 2 * Vector2.up;
+			_bottom.localPosition = _frameCenter + scaleRatio * _frameSide / 2 * Vector2.down;
+		}
 	}
 }

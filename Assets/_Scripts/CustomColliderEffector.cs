c224e1a7f05c05a2984c0288caa963fabff8f03f
selimbat <selim.tirellil@student.ecp.fr>
2019-06-01 23:46:01 +0200

    setup physics parameter for frame prefab

---
+++ Assets/_Scripts/CustomColliderEffector.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomColliderEffector : PlatformEffector2D
+{
+    public void OnCollisionEnter2D(Collision2D collision)
+	{
+
+	}
+}
